1b4ec749a16fee00e8c44dbd84c50e90e9df5c0f
Reza Aditya Sutrisno <adityareza46@gmail.com>
2025-05-17 10:34:59 +0700

    style(styles): add colorprimary

--- src/lib/styles/theme/index.ts
+++ src/lib/styles/theme/index.ts
@@ -6,6 +6,44 @@ const customTheme = createSystem(defaultConfig, {
   theme: {
     tokens: {
       fonts,
+      colors: {
+        primaryDark: {
+          value: '#1C86EE',
+        },
+        primaryLight: {
+          value: '#87CEFA',
+        },
+        primary: {
+          value: '#1E90FF',
+        },
+        secondaryDark: {
+          value: '#FF4500',
+        },
+        secondaryLight: {
+          value: '#FF7F50',
+        },
+        secondary: {
+          value: '#FF6347',
+        },
+        background: {
+          value: '#F0F0F0',
+        },
+        backgroundDark: {
+          value: '#2F4F4F',
+        },
+        backgroundLight: {
+          value: '#FFFFFF',
+        },
+        text: {
+          value: '#333333',
+        },
+        textLight: {
+          value: '#FFFFFF',
+        },
+        textDark: {
+          value: '#000000',
+        },
+      },
     },
   },
 });
