b167185dc0d1c44a5acc4305a23eb8d028f36e2c
Reza Aditya Sutrisno <adityareza46@gmail.com>
2025-05-18 20:56:04 +0700

    refactor: update project name

--- e2e/home.spec.ts
+++ e2e/home.spec.ts
@@ -7,10 +7,10 @@ import { test, expect } from '@playwright/test';
 
 /** replace with env vars if needed */
 const LOCAL_HOST_URL = 'http://localhost:3000/';
-const APP_NAME = 'nextarter-chakra';
+const APP_NAME = 'nextarter-chakra-new';
 const USE_THIS_TEMPLATE_TEXT = 'Use This Template';
 const OPEN_IN_GITHUB_TEXT = 'Open in Github';
-const REPO_URL = `https://github.com/agustinusnathaniel/${APP_NAME}`;
+const REPO_URL = `https://github.com/zayano/${APP_NAME}`;
 const TEMPLATE_URL = `${REPO_URL}/generate`;
 
 test('should shown home page', async ({ page }) => {
